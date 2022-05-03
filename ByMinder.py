acc561657732b1201e1357d952d513f395444a9e
marshmallowrabbit <96782766+marshmallowrabbit@users.noreply.github.com>
2022-05-03 19:46:39 +0200

    Update ByMinder.py

--- ByMinder.py
+++ ByMinder.py
@@ -8,6 +8,20 @@ ids = ['ftx'] #,'mexc','gateio','huobi','binance','ascendex','hitbtc','poloniex'
 
 def dump(*args):
     print(' '.join([str(arg) for arg in args]))
+    
+def test(id, symbol):
+    orderbook = id.fetch_order_book(symbol)
+    # exchange.close()
+    return orderbook
+    
+def askbid(id,symbol):
+    orderbook = test(id,symbol)
+    df = pd.DataFrame(orderbook, columns = ['asks','bids'])
+    df1 = pd.DataFrame()
+    df1[['asks','asks q']] = pd.DataFrame(df.asks.tolist(), index= df.index)
+    df1[['bids','bids q']] = pd.DataFrame(df.bids.tolist(), index= df.index)
+    askbid = df1[['asks','bids']].iloc[0]
+    return askbid
 
 proxies = [
     '',  # no proxy by default
