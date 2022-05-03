67521d3642a635399a45e62aaba87f8288b65531
marshmallowrabbit <96782766+marshmallowrabbit@users.noreply.github.com>
2022-05-03 19:38:55 +0200

    Update askbid.py

--- askbid.py
+++ askbid.py
@@ -2,21 +2,19 @@ import pandas as pd
 import asyncio
 import ccxt
 
-def test(exchange, symbol):
-    
-    temp_exchange = ccxt.exchange({'enableRateLimit': True,})
-    orderbook = temp_exchange.fetch_order_book(symbol)
+def test(id, symbol):
+    orderbook = id.fetch_order_book(symbol)
     # exchange.close()
     return orderbook
     
-def askbid(exchange,symbol):
-    orderbook = asyncio.get_event_loop().run_until_complete(test(exchange=exchange, symbol=symbol))
+def askbid(id,symbol):
+    orderbook = test(id,symbol)
     df = pd.DataFrame(orderbook, columns = ['asks','bids'])
     df1 = pd.DataFrame()
     df1[['asks','asks q']] = pd.DataFrame(df.asks.tolist(), index= df.index)
     df1[['bids','bids q']] = pd.DataFrame(df.bids.tolist(), index= df.index)
     askbid = df1[['asks','bids']].iloc[0]
     return askbid
-exchange = 'ftx'
+id = ccxt.ftx()
 symbol = 'BTC/USDT'
-print(test(exchange,symbol),askbid(exchange,symbol))
+print(askbid(id,symbol))
