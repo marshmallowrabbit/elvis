b6b6ace061ddc6462b64df2a01aa57d4bd448a8f
marshmallowrabbit <96782766+marshmallowrabbit@users.noreply.github.com>
2022-05-03 19:31:07 +0200

    Update askbid.py

--- askbid.py
+++ askbid.py
@@ -1,26 +1,22 @@
 import pandas as pd
 import asyncio
-import ccxt.async_support as ccxt  # noqa: E402
+import ccxt
 
-async def test(name_of_exchange, symbol):
-
-    exchange = ccxt.name_of_exchange({
-        'enableRateLimit': True,  # required accoding to the Manual
-    })
-
-    try:
-        orderbook = await exchange.fetch_order_book(symbol)
-        await exchange.close()
-        return orderbook
-    except ccxt.BaseError as e:
-        print(type(e).__name__, str(e), str(e.args))
-        raise e
+def test(exchange, symbol):
     
-def askbid(name_of_exchange,symbol):
-    orderbook = asyncio.get_event_loop().run_until_complete(test(name_of_exchange,symbol))
+    temp_exchange = ccxt.exchange({'enableRateLimit': True,})
+    orderbook = temp_exchange.fetch_order_book(symbol)
+    # exchange.close()
+    return orderbook
+    
+def askbid(exchange,symbol):
+    orderbook = asyncio.get_event_loop().run_until_complete(test(exchange=exchange, symbol=symbol))
     df = pd.DataFrame(orderbook, columns = ['asks','bids'])
     df1 = pd.DataFrame()
     df1[['asks','asks q']] = pd.DataFrame(df.asks.tolist(), index= df.index)
     df1[['bids','bids q']] = pd.DataFrame(df.bids.tolist(), index= df.index)
     askbid = df1[['asks','bids']].iloc[0]
     return askbid
+exchange = 'ftx'
+symbol = 'BTC/USDT'
+print(test(exchange,symbol),askbid(exchange,symbol))
