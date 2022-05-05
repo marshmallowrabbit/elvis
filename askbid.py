6d71983fa192844a69fec0cf9d7ce056d23a1e98
marshmallowrabbit <96782766+marshmallowrabbit@users.noreply.github.com>
2022-05-05 17:48:09 +0200

    Update askbid.py

--- askbid.py
+++ askbid.py
@@ -6,7 +6,10 @@ def askbid(id,symbol):
     orderbook = id.fetch_order_book(symbol, limit = 1)
     s = pd.Series(orderbook)
     s = s[['asks','bids']]
-    ask, bid = s.asks,s.bids
+    for a in s.asks:
+        ask = a[0] 
+    for b in s.bids:
+        bid = b[0]
     return ask, bid
 
 id = ccxt.ftx()
